bc5734db910559b616539fd8dd8602a920a7afbd
Ezequielcolque-cpu <eezequielcolque@gmail.com>
2025-09-01 14:32:37 -0300

    pequeña pulida a la actividad 1 y 4

--- 03 Condicionales/Tp3.py
+++ 03 Condicionales/Tp3.py
@@ -1,13 +1,13 @@
 print ("Hola")
 #Activida 1 y 4
-Edad = int (input ("Me podrías decir tu edad por favos"))
+Edad = int (input ("Me podrías decir tu edad por favor "))
 if Edad < 12:
-    print (f"Usted es un niñ@ de {Edad}")
+    print (f"Usted es un niñ@ de {Edad} años")
 elif Edad < 18:
-    print(f"Usted es un adolecente de {Edad}")
+    print(f"Usted es un adolecente de {Edad} años")
 elif Edad < 30:
-    print(f"Usted es mayor de edad, un adult@ joven de {Edad}")
+    print(f"Usted es mayor de edad, un adult@ joven de {Edad} años")
 elif Edad < 60:
-    print(f"Usted es un adult@ de {Edad}")
+    print(f"Usted es un adult@ de {Edad} años")
 else :
-    print(f"Usted es una persona mayor/abuel@ de {Edad}")
+    print(f"Usted es una persona mayor/abuel@ de {Edad} años")
