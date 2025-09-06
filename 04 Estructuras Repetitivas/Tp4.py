70eb4c49ceab71b262ecb14af476d3a67eb5d7b7
Ezequielcolque-cpu <eezequielcolque@gmail.com>
2025-09-06 01:26:34 -0300

    act 2 tp 4

--- 04 Estructuras Repetitivas/Tp4.py
+++ 04 Estructuras Repetitivas/Tp4.py
@@ -3,3 +3,11 @@ print("Hola Mundo")
 ContI = 0
 for ContI in range (1,101):
     print(ContI)
+#Actividad 2
+num = int(input("Ingrese un numero mayor de dos digitos preferiblemente: "))
+n = abs(num)
+numC = 0
+while n > 0:
+    n = n // 10
+    numC += 1
+print(f"El nuemero {num} tiene una cantidad de {numC} digitos")
