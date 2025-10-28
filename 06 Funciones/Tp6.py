fa6ca35c59ed096fabe4516ef16d335bf0dbfe67
Ezequielcolque-cpu <eezequielcolque@gmail.com>
2025-10-28 14:01:09 -0300

    act 3

--- 06 Funciones/Tp6.py
+++ 06 Funciones/Tp6.py
@@ -6,7 +6,15 @@ def imprimir_hola_mundo():
 def Saludo_usuario(nom):
     return f"Hola {nom}, como te va?"
 
+def info_personal(nom,apell,edad,resid):
+    return f"Hola {nom} {apell}, de {edad} años de {resid}"
+
 #main program
 print(imprimir_hola_mundo())
 nom = input("ingrese un nombre ")
 print(Saludo_usuario(nom))
+nom = input("ingrese un nombre ")
+apell = input("ingrese un apellido ")
+edad =int(input("Ingrese ssu edad "))
+resid = input("ingrese un residencia ")
+print(info_personal(nom, apell, edad, resid))
